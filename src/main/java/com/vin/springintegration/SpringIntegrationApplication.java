d7197560a2fbfcc1597082b71d9e9203366fc581
Vinodh Dhananjayan <Vino.14987@gmail.com>
2019-09-17 13:43:05 -0400

    MessagingTemplate Implementation

--- src/main/java/com/vin/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/vin/springintegration/SpringIntegrationApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
@@ -25,8 +26,8 @@ public class SpringIntegrationApplication implements ApplicationRunner {
     @Autowired
     private DirectChannel inputChannel;
 
-    @Autowired
-    DirectChannel outputChannel;
+//    @Autowired
+//    DirectChannel outputChannel;
 
 
     public static void main(String[] args) {
@@ -61,18 +62,23 @@ public class SpringIntegrationApplication implements ApplicationRunner {
 //            }
 //        });
 
-        outputChannel.subscribe(new MessageHandler() {
-            @Override
-            public void handleMessage(Message<?> message) throws MessagingException {
-                System.out.println(message.getPayload());
-            }
-        });
+//        outputChannel.subscribe(new MessageHandler() {
+//            @Override
+//            public void handleMessage(Message<?> message) throws MessagingException {
+//                System.out.println(message.getPayload());
+//            }
+//        });
 
         Message messageBuilderwithChannel = MessageBuilder.withPayload("Hello World from Channel example")
                 .setHeader("newHeader","newHeaderValue")
                 .build();
 
-        inputChannel.send(messageBuilderwithChannel);
+      //  inputChannel.send(messageBuilderwithChannel);
+
+
+        MessagingTemplate template = new MessagingTemplate();
+        Message outputMessage = template.sendAndReceive(inputChannel, messageBuilderwithChannel);
+        System.out.println(outputMessage.getPayload());
 
         // Sending message without channel example ends
 
